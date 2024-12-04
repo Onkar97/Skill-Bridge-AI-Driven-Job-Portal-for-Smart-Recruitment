545c93659c0424243400211f057253a7864ceda8
OnkarBhujbal15 <bhujbal.o@northeastern.edu>
2024-12-04 17:40:16 -0500

    Added PostgreSQL tables sql_init

--- src/main/java/edu/neu/csye6200/Driver.java
+++ src/main/java/edu/neu/csye6200/Driver.java
@@ -1,12 +1,10 @@
 package edu.neu.csye6200;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @RestController
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
